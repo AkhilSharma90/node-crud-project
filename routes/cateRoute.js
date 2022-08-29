cd461d038edbf2b8f4a7d29492a5e2c307192a4e
akhil <akhilsails@gmail.com>
2022-08-29 13:21:39 +0530

    server working

--- routes/cateRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const cateController = require('../controllers/cateController')
-//const { verifyUser, verifyAdmin } = require("../middleware/verifyToken"); //new import
-
-router.post('/', cateController.createCategory);
-
-router.get('/show', cateController.getCategories);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
-require("./database/db"); //import the database
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-const port = process.env.PORT || 3000;
+
+const port = 3000;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
